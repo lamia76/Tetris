2a614ee271911e34991560baf642a4fb3d7422aa
lamia76 <60038808+lamia76@users.noreply.github.com>
2020-05-06 01:51:46 +0800

    Add files via upload

---
+++ player.h
@@ -0,0 +1,16 @@
+
+class player{
+	public:
+		void GetName();//输入玩家姓名
+		void GetScore();//获取玩家当前得分 
+		void AddBlock();//底部增加带空格的一行 
+		void GetMap();//玩家得到地图 
+		
+	private:
+		string name;//玩家姓名 
+		int score();//得分
+		int blockA();//下落中的方块 
+		int blockB();//即将下落的方块 
+		int map();//玩家的的地图 
+		bool ending();//游戏是否结束 
+};
